3a5c1be83b54687bcf8869000b7adfb4462807ac
Dave Judd <52185dj@gmail.com>
2023-04-16 13:39:26 -0500

    Add styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,30 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+    },
     extend: {
       colors: {
-        'langits-bg': '#F0EDF3'
+        'langits-bg': '#F0EDF3',
+        'langits-black': '#333333',
+        'langits-blue': '#3572A5',
+        'langits-pink': '#C6538C',
+        'langits-white': '#F5F5F5'
       },
       container: {
         center: true,
         padding: '1rem',
       },
       fontFamily: {
-        oldenburg: ['Fira Sans']
+        firaSans: ['Fira Sans']
       }
     },
   },
